36b3758c10050e57839c2e2549b51eb930c8e3c9
Yan Lixin <yan.lixin@qq.com>
2017-03-23 18:12:12 +0800

    裁剪不需要的代码

--- mssql_outparam.go
+++ mssql_outparam.go
@@ -1,33 +1,9 @@
 package mssql
 
 import (
-	//"database/sql"
 	"database/sql/driver"
-	//"encoding/binary"
-	//"errors"
-	//"fmt"
-	//"io"
-	//"math"
-	//"net"
-	//"reflect"
-	//"strings"
-	//"time"
 	"golang.org/x/net/context" // use the "x/net/context" for backwards compatibility.
 )
-func Init() *MssqlDriver {
-	return &MssqlDriver{processQueryText: true}
-	//sql.Register("mssql", driverInstance)
-	//sql.Register("sqlserver", driverInstanceNoProcess)
-}
-func (d *MssqlDriver) OpenMSConn(dsn string) (*MssqlConn, error) {
-	return d.open(dsn)
-}
-func (c *MssqlConn) PrepareQuery(query string) (*MssqlStmt, error) {
-	return c.prepareContext(context.Background(), query)
-}
-func (s *MssqlStmt) SendQuery(args []namedValue) (err error) {
-	return s.sendQuery(args)
-}
 
 func (s *MssqlStmt) ExecOutPutParams(args []driver.NamedValue) (driver.Result, error) {
 	list := make([]namedValue, len(args))
